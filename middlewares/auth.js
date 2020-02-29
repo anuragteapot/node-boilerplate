4ea4e53648dcb635b2a417325e3fad3243f2035c
Anurag Kumar <30978328+Anu1601CS@users.noreply.github.com>
2020-02-29 19:44:35 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -24,7 +24,7 @@ const auth = async (req, res, next) => {
         status: true
       });
 
-      if (AccessTokenUser && AccessTokenUser._id) {
+      if (AccessTokenUser) {
         req.user = user.toJSON();
         req.accessToken = AccessTokenUser.toJSON();
         next();
